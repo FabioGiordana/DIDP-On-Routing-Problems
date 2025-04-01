ed9267bb861b562e7bd2807a1a99f4b753ceeedd
FabioGiordana <fabio.giordana1@gmail.com>
2025-04-01 17:36:40 +0200

    add plots for used vehicles

--- cvrptw/plot_results.py
+++ cvrptw/plot_results.py
@@ -12,8 +12,11 @@ def init_dict():
     return d
 
 
-def save_solutions(d, title, filename):
-    os.makedirs("Plots", exist_ok=True)
+def save_solutions(d, title, filename, vehicle):
+    dir = "Plots"
+    if vehicle:
+        dir="Plots_Vehicles"
+    os.makedirs(dir, exist_ok=True)
     plt.figure(figsize=(8, 5))
     for m in methods:
         x_vals = list(d[m].values())
@@ -25,10 +28,10 @@ def save_solutions(d, title, filename):
     plt.title(title)
     plt.legend()
     plt.grid()
-    plt.savefig(f"Plots/{filename}")
+    plt.savefig(f"{dir}/{filename}")
     plt.close()
 
-def plot_solutions(dir = "Results"):
+def plot_solutions(vehicle=True):
     all_benchmarks = init_dict()
     all_total = sum(benchmarks.values())
     all_count = 1
@@ -36,16 +39,19 @@ def plot_solutions(dir = "Results"):
         cumulative_integral = init_dict()
         total = benchmarks[benchmark]
         for i in range(1, total+1):
-            with open(f"{dir}/{benchmark}/{benchmark}{i:02}.json", "r") as file:
+            with open(f"Results/{benchmark}/{benchmark}{i:02}.json", "r") as file:
                 data = json.load(file)
                 for m in methods:
                     p_integral = data[m]["Primal Integral: "]
+                    if vehicle:
+                        v_used = data[m]["Used Vehicles: "]
+                        p_integral = p_integral*v_used
                     cumulative_integral[m][i/total] = cumulative_integral[m][(i-1)/total] + p_integral
                     all_benchmarks[m][all_count/all_total] = all_benchmarks[m][(all_count-1)/all_total] + p_integral
                 all_count += 1
         save_solutions(cumulative_integral, title = f"Primal Integral for {benchmark} benchmark instances", 
-                       filename=f"{benchmark}.pdf")
-    save_solutions(all_benchmarks, "Primal Integral on all benchmark instances", "Total.pdf")
+                       filename=f"{benchmark}.pdf", vehicle=vehicle)
+    save_solutions(all_benchmarks, "Primal Integral on all benchmark instances", "Total.pdf",vehicle)
         
 
 if __name__ == "__main__":
