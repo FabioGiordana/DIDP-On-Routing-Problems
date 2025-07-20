f04fe95b50eba70ab7941b1ce59703b2313537b1
Fabio Giordana <fabio.giordana1@gmail.com>
2025-07-20 12:23:46 +0200

    all solutions

--- mmcvrp/VrpSets/run.py
+++ mmcvrp/VrpSets/run.py
@@ -111,7 +111,7 @@ def read_instances(file_path):
 
 if __name__ == "__main__":
     time_limit = 600
-    folder = "Vrp-Set-Golden/Golden"
+    folder = "Vrp-Set-M/M"
     cp_model = CPModel()
     didp_model = DIDPModel()
     os.makedirs("Results", exist_ok=True)
@@ -122,7 +122,7 @@ if __name__ == "__main__":
         if instance["n"] > 501:
             print(f"Skipping {filepath} due to the dimension of the instance")
         else:
-            dir = f"Results/Golden"
+            dir = f"Results/M"
             os.makedirs(dir, exist_ok=True)
             if os.path.exists(f"{dir}/{filepath}.json"):
                 try:
